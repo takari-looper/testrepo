43d1540302a750ab8c01c74dae782441277a74e6
Anton Tanasenko <atg.sleepless@gmail.com>
2016-06-29 14:52:46 +0300

    asd

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,7 +3,7 @@ package test;
 public class Test {
 
   public static void main(String[] args) {
-    System.out.println("Bla6sdfsdfsdf67123");
+    System.out.println("Bla6sdfsdfsdf671234");
   }
 
 }
