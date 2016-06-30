3cec2fb1076aee08ff7b506e8a9374633232b5ae
Anton Tanasenko <atg.sleepless@gmail.com>
2016-06-30 19:21:49 +0300

    asdfsdf

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,9 +1,12 @@
 package test;
 
+import java.util.regex.Pattern;
+
 public class Test {
 
   public static void main(String[] args) {
-    System.out.println("Blaasasda");
+    System.out.println(
+        Pattern.compile("(refs/heads/|refs/remotes/|remotes/)?[^/]*\\Q/master\\E").matcher("refs/heads/master").matches());
   }
 
 }
