4298bc7a98cce821b20ddfb68f60ef3ddf5b9caa
Anton Tanasenko <atg.sleepless@gmail.com>
2016-06-30 19:23:56 +0300

    asdasd

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,12 +1,9 @@
 package test;
 
-import java.util.regex.Pattern;
-
 public class Test {
 
   public static void main(String[] args) {
-    System.out.println(
-        Pattern.compile("(refs/heads/|refs/remotes/|remotes/)?[^/]*\\Q/master\\E").matcher("refs/heads/master").matches());
+    System.out.println("bla");
   }
 
 }
